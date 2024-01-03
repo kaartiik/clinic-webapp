87ed0b73a8993cf318212cee07449be78e81aeab
Kaartiik <kaartiikofficial@gmail.com>
2024-01-03 20:01:02 +0000

    added print referral function

--- src/app/doctornotes/page.js
+++ src/app/doctornotes/page.js
@@ -1,66 +1,42 @@
 // pages/counter.js
 "use client";
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState,useRef} from 'react';
+import { useReactToPrint } from 'react-to-print';
 import globalStyles from '../styles/main.module.css';
 import QRCode from "react-qr-code";
 import styles from '../styles/DoctorNotes.module.css';
 import { collection, getDocs, Timestamp } from "firebase/firestore";
 import firebase from "../firebase";
+import Header from '@/utility/header';
 
   export default function DoctorNotes({searchParams}) {
     const appointment = searchParams;
-    console.log(JSON.stringify(searchParams, null, 2));
-    const [appointments, setAppointments] = useState([]);
-    const [clinics, setClinics] = useState([]);
+    const componentRef = useRef();
 
-    function displayFirestoreTimestamp(firestoreTimestamp) {
-      // Convert Firestore Timestamp to JavaScript Date object
-      const date = firestoreTimestamp.toDate();
-  
-      // Format the date and time
-      // const formattedDate = date.toLocaleDateString('en-US');
-      const formattedTime = date.toLocaleTimeString('en-US');
-  
-      return `${formattedTime}`;
-  }
-
-    const fetchData = async () => {
-      let clinicsArray = [];
-      let appointmentsArray = [];
-      const querySnapshot = await getDocs(collection(firebase.db, "clinics"));
-      querySnapshot.forEach((doc) => {
-          clinicsArray.push({label:doc.data().clinic_name, value:doc.data().clinic_id, openingTime:doc.data().opening_time, closingTime:doc.data().closing_time});
-        });
-
-      setClinics(clinicsArray);
-      const appointmentSnapshot = await getDocs(collection(firebase.db, "clinics", clinicsArray[0].value, "appointments"));
-
-      appointmentSnapshot.forEach((doc) => {
-        appointmentsArray.push({
-          ...doc.data(), 
-          appointment_time: displayFirestoreTimestamp(doc.data().appointment_date)
-        });
-      });
-      
-      setAppointments(appointmentsArray);
-  };
+    const handlePrint = useReactToPrint({
+      content: () => componentRef.current,
+    });
 
   useEffect(() =>{
   // fetchData();
   }, [])
 
-
   return (
     <div className={styles.container}>
-      <div className={styles.noteItem}>Patient Name: {appointment.patient_name}</div>
-      <div className={styles.noteItem}>Appointment Date: {appointment.appointment_date}</div>
-      <div className={styles.noteItem}>Medical Info: {appointment.medical_info}</div>
-      <div className={styles.noteItem}>Doctor Notes: {appointment.doctor_notes}</div>
-      <div className={styles.noteItem}>Medications: {appointment.medications.join(", ")}</div>
+      <Header title="Doctor Notes"/>
+      <div className={styles.notesContainer}>
+        <div className={styles.noteItem}>Patient Name: {appointment.patient_name}</div>
+        <div className={styles.noteItem}>Appointment Date: {appointment.appointment_date}</div>
+        <div className={styles.noteItem}>Medical Info: {appointment.medical_info}</div>
+        <div className={styles.noteItem}>Doctor Notes: {appointment.doctor_notes}</div>
+        <div className={styles.noteItem}>Medications: {appointment.medications.join(", ")}</div>
+        <div className={styles.noteItem}>Referral Letter:</div>
+        <div ref={componentRef} className={styles.noteItem}>{appointment.referral}</div>
 
-      {appointment.hasOwnProperty('referral') && 
-        <button className={globalStyles.roundedButton}>Print Referral</button>
-      }
+        {appointment.hasOwnProperty('referral') && 
+          <button className={globalStyles.roundedButton} onClick={handlePrint}>Print Referral</button>
+        }
+      </div>
     </div>
   );
 }
